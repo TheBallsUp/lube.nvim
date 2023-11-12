85fa980db527c828c47198289522c3f9bca0a7a3
dharmx <dharmx.dev@gmail.com>
2023-11-13 01:37:28 +0530

    feat: something terrible happened here

--- plugin/lube.lua
+++ plugin/lube.lua
@@ -1,6 +1,2 @@
-if vim.version().minor < 9 then
-  vim.notify("lube.nvim requires at least nvim-0.9.0.")
-  return
-end
-
+if vim.version().minor < 9 then return vim.notify("lube.nvim requires at least nvim-0.9.0.") end
 if vim.g.loaded_lube ~= 1 then vim.g.loaded_lube = 1 end
